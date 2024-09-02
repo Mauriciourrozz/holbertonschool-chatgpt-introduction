65880ee068b4d233ebea5f3adb09affdb6f8f14b
Mauriciourrozz <9366@holbertonstudents.com>
2024-09-02 15:27:01 +0000

    add factorial files

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,13 +5,13 @@ def factorial(n):
         n -= 1
     return result
 
-
-try:
-    number = int(input("Ingrese un número para calcular su factorial: "))
-    if number < 0:
-        print("El factorial no está definido para números negativos.")
-    else:
-        f = factorial(number)
-        print(f"El factorial de {number} es {f}.")
-except ValueError:
-    print("Por favor, ingrese un número entero válido.")
+if __name__ == "__main__":
+    try:
+        number = int(input("Ingrese un número para calcular su factorial: "))
+        if number < 0:
+            print("El factorial no está definido para números negativos.")
+        else:
+            f = factorial(number)
+            print(f"El factorial de {number} es {f}.")
+    except ValueError:
+        print("Por favor, ingrese un número entero válido.")
